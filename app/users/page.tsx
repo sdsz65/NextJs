576674ca7868bcfc78e9b6bd76e00b6aa1644a0c
Davood <mac@macs-MacBook-Pro.local>
2024-11-09 23:42:59 +0330

    add sort to users list

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,37 +1,17 @@
-import React from 'react'
 
-interface User {
-    id: number;
-    name: string
-    email: string
+import React from 'react'
+import UserTable from './UserTable';
+
+interface Props {
+    searchParams: { sortOrder: string }
 }
 
-const UsersPage = async () => {
-    const res = await fetch(
-        'https://jsonplaceholder.typicode.com/users',
-        {
-            cache: 'no-store', //For disabling caching at all
-            // next: { revalidate: 10 } // For get Fresh data every 10 seconds
-        }
-    )
-    const users: User[] = await res.json()
+const UsersPage = ({ searchParams: { sortOrder } }: Props) => {
+    
     return (
         <>
             <h1>Users</h1>
-            <table className='table table-bordered'>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Email</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {users.map(user => <tr key={user.id}>
-                        <td>{user.name}</td>
-                        <td>{user.email}</td>
-                    </tr>)}
-                </tbody>
-            </table>
+            <UserTable sortOrder={sortOrder} />
         </>
     )
 }
